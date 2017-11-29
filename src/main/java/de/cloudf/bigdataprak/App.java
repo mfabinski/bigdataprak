2e4feefe5861437fd571d37994ae406e1a6eef2e
Matthias Fabinski <matthias.fab@gmail.com>
2017-11-29 17:02:55 +0100

    initial setup

---
+++ src/main/java/de/cloudf/bigdataprak/App.java
@@ -0,0 +1,24 @@
+package de.cloudf.bigdataprak;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
